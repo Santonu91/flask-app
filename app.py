930a2b94b17166e37f849047690371d4afd5023d
Santonu91 <91974759+Santonu91@users.noreply.github.com>
2024-07-16 18:21:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,4 +11,10 @@ def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    
+#    app.run(debug=True)
+
+# By default, Flask binds to 127.0.0.1, which makes the server 
+#    only accessible from the local machine. To make it accessible 
+#    from other machines, you need to bind it to 0.0.0.0  
+    app.run(debug=True, host='0.0.0.0')
